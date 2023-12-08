f7c30a41f593ffebb820502f1ae8389e71e6fe43
David Dieperink <david.dieperink@ik.me>
2023-12-08 10:37:46 +0100

    created function to convert semicircles to degrees

--- main.php
+++ main.php
@@ -13,6 +13,11 @@ fclose($f);
 // echo '<pre>';
 // var_dump($data[0]);
 
+$datetimeFormat = 'H:i:s';
+$date = new DateTime();
+$date->setTimestamp('1070881939');
+echo ($date->format($datetimeFormat));
+
 echo "<table><tbody>";
 foreach ($data as $values) {
 	foreach ($values as $v) {
@@ -21,3 +26,22 @@ foreach ($data as $values) {
 	echo "</tr>";
 }
 echo "</thead></table>";
+
+/**
+ * Function to convert semicircles to degrees
+ * @param string $semicircles - semicircles value
+ * @return float semicircles converted in degrees
+ */
+function semicirclesToDegrees($semicircles)
+{
+	$degrees = ($semicircles * 180) / pow(2, 31);
+	return $degrees;
+}
+
+$latitudeSemicircles = 554902588;
+$latitudeDegrees = semicirclesToDegrees($latitudeSemicircles);
+echo "Latitude in degrees : " . $latitudeDegrees . "<br>";
+
+$longitudeSemicircles = 75227728;
+$longitudeDegrees = semicirclesToDegrees($longitudeSemicircles);
+echo "Longitude in degrees : " . $longitudeDegrees;
