4d30ff31e17d9715d1fede11759a864693689adb
David Dieperink <david.dieperink@ik.me>
2023-12-11 10:18:25 +0100

    added lap data parsing

--- main.php
+++ main.php
@@ -1,4 +1,6 @@
 <?php
+
+// Open file and get data
 $filename = 'activity/vma-s5-4_data_lap.csv';
 $data = [];
 
@@ -13,11 +15,6 @@ fclose($f);
 echo '<pre>';
 var_dump($data[0]);
 
-$datetimeFormat = 'H:i:s';
-$date = new DateTime();
-$date->setTimestamp('1070881939');
-echo ($date->format($datetimeFormat));
-
 echo "<table><tbody>";
 foreach ($data as $values) {
 	foreach ($values as $v) {
@@ -25,7 +22,7 @@ foreach ($data as $values) {
 	}
 	echo "</tr>";
 }
-echo "</thead></table>";
+echo "</tbody></table>";
 
 $lap_timestamp = [];
 $lap_startTime = [];
@@ -33,21 +30,57 @@ $lap_startPositionLat = [];
 $lap_startPositionLong = [];
 $lap_endPositionLat = [];
 $lap_endPositionLong = [];
+$lap_totalElapsedTime = [];
+$lap_totalDistance = [];
 
+parseLapData($data);
+
+/**
+ * Function to parse lap data
+ * @param array $data - array of data
+ */
 function parseLapData($data)
 {
-	global $lap_timestamp, $lap_startTime, $lap_startPositionLat, $lap_startPositionLong, $lap_endPositionLat, $lap_endPositionLong;
+	$datetimeFormat = 'H:i:s';
+	global
+		$lap_timestamp,
+		$lap_startTime,
+		$lap_startPositionLat,
+		$lap_startPositionLong,
+		$lap_endPositionLat,
+		$lap_endPositionLong,
+		$lap_totalElapsedTime,
+		$lap_totalDistance;
+
+	$date = new DateTime();
 
 	for ($i = 0; $i < count($data); $i++) {
 		if (isset($data[$i])) {
 			if (isset($data[$i][0]) && is_numeric($data[$i][0])) {
-				$lap_timestamp[] = $data[$i][0];
+				$date->setTimestamp($data[$i][0]);
+				$lap_timestamp[] = $date->format($datetimeFormat);
 			}
 			if (isset($data[$i][1]) && is_numeric($data[$i][1])) {
-				$lap_startTime[] = $data[$i][1];
+				$date->setTimestamp($data[$i][1]);
+				$lap_startTime[] = $date->format($datetimeFormat);
 			}
 			if (isset($data[$i][2]) && is_numeric($data[$i][2])) {
-				$lap_startPositionLat[] = $data[$i][2];
+				$lap_startPositionLat[] = semicirclesToDegrees($data[$i][2]);
+			}
+			if (isset($data[$i][3]) && is_numeric($data[$i][3])) {
+				$lap_startPositionLong[] = semicirclesToDegrees($data[$i][3]);
+			}
+			if (isset($data[$i][4]) && is_numeric($data[$i][4])) {
+				$lap_endPositionLat[] = semicirclesToDegrees($data[$i][4]);
+			}
+			if (isset($data[$i][5]) && is_numeric($data[$i][5])) {
+				$lap_endPositionLong[] = semicirclesToDegrees($data[$i][5]);
+			}
+			if (isset($data[$i][6]) && is_numeric($data[$i][6])) {
+				$lap_totalElapsedTime[] = $data[$i][6];
+			}
+			if (isset($data[$i][8]) && is_numeric($data[$i][8])) {
+				$lap_totalDistance[] = $data[$i][8];
 			}
 		} else {
 			return;
@@ -55,11 +88,8 @@ function parseLapData($data)
 	}
 }
 
-var_dump($lap_timestamp);
-
-var_dump($lap_startTime);
-var_dump($lap_startPositionLat);
-
+var_dump($lap_totalElapsedTime);
+var_dump($lap_totalDistance);
 
 /**
  * Function to convert semicircles to degrees
@@ -71,11 +101,3 @@ function semicirclesToDegrees($semicircles)
 	$degrees = ($semicircles * 180) / pow(2, 31);
 	return $degrees;
 }
-
-$latitudeSemicircles = 554902588;
-$latitudeDegrees = semicirclesToDegrees($latitudeSemicircles);
-echo "Latitude in degrees : " . $latitudeDegrees . "<br>";
-
-$longitudeSemicircles = 75227728;
-$longitudeDegrees = semicirclesToDegrees($longitudeSemicircles);
-echo "Longitude in degrees : " . $longitudeDegrees;
